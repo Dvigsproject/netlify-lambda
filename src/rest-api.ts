b13d5cc005a55de6ec4f705809c736feb3856364
Dvigsproject <60187097+Dvigsproject@users.noreply.github.com>
2021-06-04 10:15:21 +0300

    +=

--- src/rest-api.ts
+++ src/rest-api.ts
@@ -1,7 +1,7 @@
 
 import { APIGatewayEvent, Context } from 'aws-lambda'
 
-export default async function handler (event: APIGatewayEvent, context: Context) {
+export default async function handler ({event: APIGatewayEvent, context: Context}) {
   return {
     status: 200,
     body: JSON.stringify({
